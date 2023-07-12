595863bf0c6a28dd32de8d55240b67c736057e7c
Mr-Struck <66813999+Mr-Struck@users.noreply.github.com>
2023-07-12 12:33:43 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+// import { MuiYouTubeForm } from "./components/MuiYouTubeForm";
+import { YouTubeForm } from "./components/YouTubeForm";
+// import { YupYouTubeForm } from "./components/YupYouTubeForm";
+
+function App() {
+  return (
+    <div>
+      <YouTubeForm />
+      {/* <YupYouTubeForm /> */}
+      {/* <MuiYouTubeForm /> */}
+    </div>
+  );
+}
+
+export default App;
